d7fa8653169bbaf69df948ba0016b8b40e119332
diuguide <everett.diuguid@gmail.com>
2021-05-03 21:54:04 -0400

    created NewUser class

---
+++ src/main/java/com/revature/project_0/models/NewUser.java
@@ -0,0 +1,51 @@
+package com.revature.project_0.models;
+
+public class NewUser {
+
+    // NewUser login info
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+
+    // NewUser constructor
+    public NewUser(String firstName, String lastName, String username, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
