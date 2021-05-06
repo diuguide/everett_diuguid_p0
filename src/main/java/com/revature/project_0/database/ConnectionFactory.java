0ae0f559b059795784a3c70603ded287007e04ca
diuguide <everett.diuguid@gmail.com>
2021-05-06 10:39:42 -0400

    created resources folder, and application properties, finished connection factory component

--- src/main/java/com/revature/project_0/database/ConnectionFactory.java
+++ src/main/java/com/revature/project_0/database/ConnectionFactory.java
@@ -1,15 +1,36 @@
 package com.revature.project_0.database;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionFactory {
 
     private static ConnectionFactory connectionFactory;
+    private Properties props = new Properties();
 
-    private ConnectionFactory() {
-
+    // Load driver into jvm
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
+    // Grab credentials from properties file
+    private ConnectionFactory() {
+        try {
+            props.load(new FileReader("src/main/resources/application.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // If no instance of ConnectionFactory exists, create one
     public static ConnectionFactory getInstance() {
         if (connectionFactory == null) {
             connectionFactory = new ConnectionFactory();
@@ -17,7 +38,22 @@ public class ConnectionFactory {
         return connectionFactory;
     }
 
+    // create sql connection
     public Connection getConnection() {
-        return null;
+        Connection conn = null;
+        try {
+
+            conn = DriverManager.getConnection(
+                    props.getProperty("host-url"),
+                    props.getProperty("username"),
+                    props.getProperty("password"));
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+
+        }
+
+        return conn;
     }
 }
