e60c78eee7be67714b906ce0f0dcb268df0abddd
diuguide <everett.diuguid@gmail.com>
2021-05-06 07:27:14 -0400

    screen router component

---
+++ src/main/java/com/revature/project_0/util/ScreenRouter.java
@@ -0,0 +1,23 @@
+package com.revature.project_0.util;
+
+import com.revature.project_0.screens.Screen;
+
+public class ScreenRouter {
+
+    private LinkedList<Screen> screens = new LinkedList<>();
+
+    public ScreenRouter addScreen(Screen screen) {
+        screens.add(screen);
+        return this;
+    }
+
+    public void navigate(String route) {
+        for (int i = 0; i < screens.size(); i++) {
+            Screen screen = screens.get(i);
+            if ( screen.getRoute().equals(route)) {
+                screen.render();
+            }
+        }
+    }
+
+}
